6854a918ce3a71ad0ecbe6eaf2cd72e15e534754
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2020-10-20 20:40:45 -0500

    Initial route configuration and creation of mutating component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MutationComponent } from './mutation/mutation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: '/mutation', pathMatch: 'full'},
+	{ path: 'mutation', component: MutationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MutationComponent } from './mutation/mutation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MutationComponent
   ],
   imports: [
     BrowserModule,
