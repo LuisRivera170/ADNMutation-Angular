2885aab76945898d97a3849b48c2e2a702a47069
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2020-10-22 21:19:30 -0500

    Config file

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 export const URL_BACKEND = 'http://localhost:8080';
 
-export const CLIENT_JWT_CREDENTIALS = btoa('client-id:client-password');
+export const CLIENT_JWT_CREDENTIALS = btoa('mutating-human-client:hARy7*bwl81YCS')
